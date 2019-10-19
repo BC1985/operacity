9daf534a671345e8302fe46e00176730fb1632cd
Ben Cohen <benjcohen1985@gmail.com>
2019-10-19 13:27:45 -0400

    Change class to id

--- src/Nav/NavToggleButton.js
+++ src/Nav/NavToggleButton.js
@@ -3,10 +3,10 @@ import "./NavToggleButton.css";
 
 function NavToggleButton(props) {
   return (
-    <div className="hamburger-icon" onClick={props.openSideNav}>
-      <div className="line1"></div>
-      <div className="line2"></div>
-      <div className="line3"></div>
+    <div id="hamburger-icon" onClick={props.openSideNav}>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
