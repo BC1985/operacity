6fc96a1a8820f87c5e4fbb0798757c387a66be51
Ben Cohen <benjcohen1985@gmail.com>
2019-10-19 19:29:17 -0400

    Apply conditional styling to sidenav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./Nav.css";
 
 function Nav(props) {
+  let sideNavClass = "nav-links";
+  if (props.show) {
+    sideNavClass = "nav-links open";
+  }
   return (
     <nav className="nav">
-      <ul className="nav-links">
+      <ul className={sideNavClass}>
         <li>About</li>
         <li>Program</li>
         <li>Gallery</li>
