4b602a94f15338ca9d8401ed927ca3a521515223
Ben Cohen <benjcohen1985@gmail.com>
2019-10-19 08:27:36 -0400

    Import header component

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Header from "../Header/Header";
 
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
