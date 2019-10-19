282c41f061a34a2bdca94a006ac7cd4102aa9c6e
Ben Cohen <benjcohen1985@gmail.com>
2019-10-19 11:05:22 -0400

    Import Nav component

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Header from "../Header/Header";
-
+import Nav from "../Nav/Nav";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <Header />
       </div>
     );
