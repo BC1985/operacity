4d2d003cf6297c1e4ceb357154f59542de8639ab
Ben Cohen <benjcohen1985@gmail.com>
2019-10-19 15:56:50 -0400

    Add styles and function for icon animation

--- src/Nav/NavToggleButton.js
+++ src/Nav/NavToggleButton.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import "./NavToggleButton.css";
 
-function NavToggleButton(props) {
-  return (
-    <div id="hamburger-icon" onClick={props.openSideNav}>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+class NavToggleButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+  openSideNav = () => {
+    this.setState(prevState => {
+      return { isOpen: !prevState.isOpen };
+    });
+  };
+  render() {
+    const styles = {
+      hamburgerIcon: {
+        position: "absolute",
+        padding: "20px",
+        zIndex: 2,
+        cursor: "pointer"
+      },
+      line: {
+        height: "3px",
+        width: "20px",
+        background: "white",
+        transition: "all 0.3s ease"
+      },
+      lineTop: {
+        transform: this.state.isOpen ? "rotate(45deg)" : "none",
+        transformOrigin: "top left",
+        marginBottom: "5px"
+      },
+      lineMiddle: {
+        opacity: this.state.isOpen ? 0 : 1,
+        transform: this.props.isOpen ? "translateX(-16px)" : "none"
+      },
+      lineBottom: {
+        transform: this.state.isOpen
+          ? "translateX(-1px) rotate(-45deg)"
+          : "none",
+        transformOrigin: "top left",
+        marginTop: "5px"
+      }
+    };
+    return (
+      <div>
+        <div style={{ ...styles.hamburgerIcon }} onClick={this.openSideNav}>
+          <div style={{ ...styles.line, ...styles.lineTop }} />
+          <div style={{ ...styles.line, ...styles.lineMiddle }} />
+          <div style={{ ...styles.line, ...styles.lineBottom }} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavToggleButton;
