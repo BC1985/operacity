45cc915a0a4215522a2cff282af2d00a4e8cf21a
Ben Cohen <benjcohen1985@gmail.com>
2019-10-19 11:06:32 -0400

    Create nav component

---
+++ src/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <ul className="nav-links">
+        <li>About</li>
+        <li>Program</li>
+        <li>Gallery</li>
+        <li>Tickets</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
