d4a4bfc4e6b5863acb5d2f73b8b769dc04862c6a
Ben Cohen <benjcohen1985@gmail.com>
2019-10-19 12:47:24 -0400

    Conditionally render n nav on click

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import Nav from "../Nav/Nav";
 import "./App.css";
+import NavToggleButton from "../Nav/NavToggleButton";
 
 class App extends Component {
+  state = {
+    isSideNavOpen: false
+  };
+  openSideNav = () => {
+    this.setState(prevState => {
+      return { isSideNavOpen: !prevState.isSideNavOpen };
+    });
+  };
   render() {
     return (
       <div>
-        <Nav />
+        <NavToggleButton openSideNav={this.openSideNav} />
+        {this.state.isSideNavOpen && <Nav />}
         <Header />
       </div>
     );
