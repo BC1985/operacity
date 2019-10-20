8a24b8b74f37fa2ea85d59348c118808927bc5ae
Ben Cohen <benjcohen1985@gmail.com>
2019-10-20 18:40:16 -0400

    Break text to paragraphs

--- src/About/About.js
+++ src/About/About.js
@@ -7,26 +7,37 @@ function About() {
       <div className="container">
         <h1>About</h1>
         <img src={conductor} alt="conductor" />
-        <h2>Lorem Ipsum Dolor Sit Amet</h2>
-        <p className="text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam
-          quam et condimentum condimentum. Aliquam volutpat accumsan lacus vel
-          tincidunt. Morbi cursus pulvinar lorem non eleifend. In hac habitasse
-          platea dictumst. Suspendisse potenti. Suspendisse dui leo, volutpat a
-          dictum dapibus, fringilla sed nisi. Vestibulum vehicula, odio ut
-          mollis bibendum, ex urna luctus ligula, eget mattis nunc orci et sem.
-          Nam pulvinar elit pulvinar imperdiet bibendum. Integer orci ex,
-          ultrices non nibh eu, venenatis blandit eros. Donec id turpis
-          ullamcorper, maximus massa nec, aliquam magna. Curabitur porta justo
-          dui, eget tempus turpis commodo a. Pellentesque habitant morbi
-          tristique senectus et netus et malesuada fames ac turpis egestas.
-          Mauris interdum elit est, non lacinia justo blandit eget. Etiam
-          tincidunt vehicula odio convallis ornare. Duis sollicitudin est vitae
-          congue congue. Integer tempus volutpat turpis, vitae tristique felis.
-          Ut dapibus orci id dapibus pulvinar. Mauris tristique quis turpis id
-          maximus. Pellentesque mi nisi, finibus quis fringilla in, dapibus vel
-          augue.
+        <p className="credit">
+          Image credit-Taiwan National Symphony Orchestra website
         </p>
+        <h2>Lorem Ipsum Dolor Sit Amet</h2>
+        <div className="text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            aliquam quam et condimentum condimentum. Aliquam volutpat accumsan
+            lacus vel tincidunt.
+          </p>
+          <p>
+            Morbi cursus pulvinar lorem non eleifend. In hac habitasse platea
+            dictumst. Suspendisse potenti. Suspendisse dui leo, volutpat a
+            dictum dapibus, fringilla sed nisi. Vestibulum vehicula, odio ut
+            mollis bibendum, ex urna luctus ligula, eget mattis nunc orci et
+            sem. Nam pulvinar elit pulvinar imperdiet bibendum. Integer orci ex,
+            ultrices non nibh eu, venenatis blandit eros. Donec id turpis
+            ullamcorper, maximus massa nec, aliquam magna. Curabitur porta justo
+            dui, eget tempus turpis commodo.
+          </p>
+          <p>
+            {" "}
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Mauris interdum elit est, non lacinia justo
+            blandit eget. Etiam tincidunt vehicula odio convallis ornare. Duis
+            sollicitudin est vitae congue congue. Integer tempus volutpat
+            turpis, vitae tristique felis. Ut dapibus orci id dapibus pulvinar.
+            Mauris tristique quis turpis id maximus. Pellentesque mi nisi,
+            finibus quis fringilla in, dapibus vel augue.
+          </p>
+        </div>
       </div>
     </>
   );
