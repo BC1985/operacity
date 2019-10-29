86ac34dee4497c0c1ca44644127e7bdbacb035d7
Ben Cohen <benjcohen1985@gmail.com>
2019-10-29 18:53:34 -0400

    Attach props

--- src/Nav/NavToggleButton.js
+++ src/Nav/NavToggleButton.js
@@ -2,18 +2,8 @@ import React, { Component } from "react";
 import "./NavToggleButton.css";
 
 class NavToggleButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    };
-  }
-  openSideNav = () => {
-    this.setState(prevState => {
-      return { isOpen: !prevState.isOpen };
-    });
-  };
   render() {
+    const { isSideNavOpen, openSideNav } = this.props;
     const styles = {
       hamburgerIcon: {
         position: "absolute",
@@ -28,25 +18,23 @@ class NavToggleButton extends Component {
         transition: "all 0.3s ease"
       },
       lineTop: {
-        transform: this.props.isSideNavOpen ? "rotate(50deg)" : "none",
+        transform: isSideNavOpen ? "rotate(50deg)" : "none",
         transformOrigin: "top left",
         marginBottom: "5px"
       },
       lineMiddle: {
-        opacity: this.props.isSideNavOpen ? 0 : 1,
-        transform: this.props.isSideNavOpen ? "translateX(-16px)" : "none"
+        opacity: isSideNavOpen ? 0 : 1,
+        transform: isSideNavOpen ? "translateX(-16px)" : "none"
       },
       lineBottom: {
-        transform: this.props.isSideNavOpen
-          ? "translateX(-1px) rotate(-50deg)"
-          : "none",
+        transform: isSideNavOpen ? "translateX(-1px) rotate(-50deg)" : "none",
         transformOrigin: "top left",
         marginTop: "5px"
       }
     };
     return (
       <div>
-        <div id="hamburger-icon" onClick={this.props.openSideNav}>
+        <div id="hamburger-icon" onClick={openSideNav}>
           <div style={{ ...styles.line, ...styles.lineTop }} />
           <div style={{ ...styles.line, ...styles.lineMiddle }} />
           <div style={{ ...styles.line, ...styles.lineBottom }} />
