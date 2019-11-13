5395384ebef6f0433c9bec8b6a181908985db506
Ben Cohen <benjcohen1985@gmail.com>
2019-11-13 16:08:55 -0500

    Apply array method for rendering thumbnails

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./Gallery.css";
 import Thumbnail from "../Thumbnail/Thumbnail";
-import opera from "../Images/images.jpeg";
-import verdi from "../Images/verdi.jpg";
-import ensemble from "../Images/opera2.jpg";
 
-function Gallery(props) {
+function Gallery() {
+  let urls = [
+    "https://live.staticflickr.com/65535/47710839562_0574dfd097_z.jpg",
+    "https://media.tacdn.com/media/attractions-splice-spp-674x446/07/69/50/2f.jpg",
+    "https://cdn.britannica.com/14/166714-050-454513F5/Performance-trovatore-Il-Giuseppe-Verdi-2011.jpg",
+    "https://www.airfrance.co.uk/GB/common/common/img/tbaf/news/NCE/l-opera-de-nice-la-traviatta-dans-son-decor-d-origine/NCE-l-opera-de-nice-la-traviatta-dans-son-decor-d-origine-2_1-1024x512.jpg"
+  ];
   return (
-    <div className="wrapper">
+    <div className="gallery-wrapper">
       <h1 className="gallery">Gallery</h1>
       <div className="thumbnails-container">
-        <Thumbnail image={opera} />
-        <Thumbnail image={verdi} />
-        <Thumbnail image={ensemble} />
+        {urls.map(thumbnail => (
+          <Thumbnail image={thumbnail} key={thumbnail} />
+        ))}
       </div>
     </div>
   );
