7e039240e9f5dceb98cd13ddec6ee133f80c8060
Ben Cohen <benjcohen1985@gmail.com>
2019-11-24 21:46:12 -0500

    Apply classes and add composer name

--- src/Schedule/Date.js
+++ src/Schedule/Date.js
@@ -3,14 +3,23 @@ import "./Date.css";
 function Date(props) {
   const info = props.programInfo.map((info, index) => (
     <div className="info" key={index}>
-      <h1 className="date">{info.date}</h1>
-      <p>{info.operaName}</p>
-      <p>{info.time}</p>
-      <p>{info.soloists}</p>
+      <p className="date">{info.date}</p>
+      <h3 className="composer">{info.composer}</h3>
+      <h2 className="opera-name">{info.operaName}</h2>
+      <p className="time">{info.time}</p>
+      <p className="soloists">{info.soloists}</p>
+      <button className="tickets-btn">Get tickets</button>
       <hr />
     </div>
   ));
-  return <div className="date-wrapper">{info}</div>;
+  return (
+    <div className="date-wrapper">
+      <div className="program-header">
+        <h1>December 2019</h1>
+      </div>
+      {info}
+    </div>
+  );
 }
 
 export default Date;
