8e6bde293c1e1d838014d2d6afe6b65db1ba018a
Ben Cohen <benjcohen1985@gmail.com>
2019-11-25 13:05:09 -0500

    Clean up divs

--- src/Schedule/Date.js
+++ src/Schedule/Date.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./Date.css";
 function Date(props) {
   const info = props.programInfo.map((info, index) => (
-    <div className="info" key={index}>
+    <div className="info-container" key={index}>
       <p className="date">{info.date}</p>
-      <h3 className="composer">{info.composer}</h3>
-      <h2 className="opera-name">{info.operaName}</h2>
-      <p className="time">{info.time}</p>
-      <p className="soloists">{info.soloists}</p>
-      <button className="tickets-btn">Get tickets</button>
-      <hr />
+      <div className="main-info">
+        <h3 className="composer">{info.composer}</h3>
+        <h2 className="opera-name">{info.operaName}</h2>
+        <p className="time">{info.time}</p>
+        <p className="soloists">{info.soloists}</p>
+        <button className="tickets-btn">Get tickets</button>
+        <hr />
+      </div>
     </div>
   ));
   return (
@@ -17,7 +19,8 @@ function Date(props) {
       <div className="program-header">
         <h1>December 2019</h1>
       </div>
-      <div className="info-container">{info}</div>
+      {info}
+      <div className="program-shape"></div>
     </div>
   );
 }
