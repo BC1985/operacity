df6fc47109335043a5e785a44f0dafbd96be9cd8
Ben Cohen <benjcohen1985@gmail.com>
2019-12-17 12:49:45 -0500

    Add interface for changing ticket quantity Implement tooltip logic on quantity change

--- src/TicketsPurchaseForm/TicketsPurchaseForm.js
+++ src/TicketsPurchaseForm/TicketsPurchaseForm.js
@@ -2,79 +2,94 @@ import React, { useState } from "react";
 import "./TicketsPurchaseForm.css";
 
 function Tickets(props) {
-  // commented so no errors show on refresh
-  // const operaName = props.location.selectedShow[0];
-  // const composer = props.location.selectedShow[1];
-  // const showDate = props.location.selectedShow[2];
-  // const showTime = props.location.selectedShow[3];
-  // const infoContainerImage = props.location.selectedShow[4];
+  const operaName = props.location.selectedShow[0];
+  const composer = props.location.selectedShow[1];
+  const showDate = props.location.selectedShow[2];
+  const showTime = props.location.selectedShow[3];
+  const infoContainerImage = props.location.selectedShow[4];
 
-  const [ticketType, setTicketType] = useState([
-    { type: "Regular", count: 0, price: 30 },
-    { type: "Child", count: 0, price: 20 },
-    { type: "Senior", count: 0, price: 20 }
-  ]);
-  const buttonStyle = {
-    padding: "10px",
-    background: "lightGray",
-    margin: "5px",
-    width: "150px"
+  const ticketType = [{ type: "Regular", count: 0, price: 30 }];
+  const [count, setCount] = useState(0);
+  const [addButtonClick, setAddButtonClick] = useState(false);
+  const [removeButtonClick, setRemoveButtonClick] = useState(false);
+
+  const addClickHandler = () => {
+    setCount(count + 1);
+    setAddButtonClick(true);
+    setRemoveButtonClick(false);
   };
 
-  const increment = () => {
-    /*  Need logic for setTicketType that applies only to the "count" property 
-   associated with the button that was cliked */
+  const removeClickHandler = () => {
+    setCount(count - 1);
+    setRemoveButtonClick(true);
+    setAddButtonClick(false);
   };
-  const decrement = () => {
-    /*  Need logic for setTicketType that applies only to the "count" property 
-   associated with the button that was cliked */
-  };
-
-  const TicketTypes = ({ ticketType }) => (
-    <ul className="ticket-types">
-      {ticketType.map((ticket, i) => (
-        <li key={i}>
-          {ticket.type}- ${ticket.price}
-        </li>
-      ))}
-    </ul>
-  );
   const NumberOfTickets = ({ ticketType }) => (
-    <div className="tickets-quantity">
+    <div className="ticket-container">
+      <h1>Get your ticket today!</h1>
       {ticketType.map((ticket, i) => (
-        <div key={i}>
-          <h3>{ticket.type} </h3>
-          <button onClick={increment} style={buttonStyle}>
-            +
-          </button>
-          <button onClick={decrement} style={buttonStyle}>
-            -
-          </button>
-          <h3>{ticketType[i].count}</h3>
+        <div key={ticket.type}>
+          <h3>
+            {ticket.type}- ${ticket.price}
+            <button
+              onClick={addClickHandler}
+              className={count > 0 ? "added-to-cart " : "ticket-btn-default"}
+            >
+              {count > 0 ? "Added!" : "Add to cart"}
+            </button>
+            {count > 0 && (
+              <button className="remove-button" onClick={removeClickHandler}>
+                Remove
+              </button>
+            )}
+          </h3>
         </div>
       ))}
     </div>
   );
+
+  const TooltipAdd = () => (
+    <div className="animation-box">
+      <div className="tooltip-add">Added to cart</div>
+    </div>
+  );
+  const TooltipRemove = () => (
+    <div className="animation-box">
+      <div className="tooltip-remove">Removed from cart</div>
+    </div>
+  );
+  function CartItem() {
+    return (
+      <div className="cart">
+        <h2>Checkout</h2>
+        <h3>Quantity: {count}</h3>
+        <p>Total: ${count * 30}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="tickets-wrapper">
       <div
         className="cropped-image-container"
-        // style={{ backgroundImage: `url(${infoContainerImage})` }}
+        style={{ backgroundImage: `url(${infoContainerImage})` }}
       >
         <div className="image-overlay"></div>
       </div>
 
       <div className="show-info">
-        <h2 className="inner_composer">Beethoven's</h2>
-        <h1 className="inner_opera-name">Fidelio</h1>
-        <p className="inner_show-date">December 25</p>
-        <p className="inner_show-time">8pm</p>
+        <h2 className="inner_composer">{composer}</h2>
+        <h1 className="inner_opera-name">{operaName}</h1>
+        <p className="inner_show-date">{showDate}</p>
+        <p className="inner_show-time">{showTime}</p>
       </div>
-      <form className="form-container">
-        <TicketTypes ticketType={ticketType} />
+      <div>
         <NumberOfTickets ticketType={ticketType} />
+        <CartItem />
         <button className="tickets-btn">Purchase tickets</button>
-      </form>
+      </div>
+      {addButtonClick === true && <TooltipAdd />}
+      {removeButtonClick === true && <TooltipRemove />}
     </div>
   );
 }
