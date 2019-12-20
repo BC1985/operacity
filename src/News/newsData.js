96cad397562c760f5fbab946048cae87122a03c1
Ben Cohen <benjcohen1985@gmail.com>
2019-12-20 12:41:18 -0500

    Create data file

---
+++ src/News/newsData.js
@@ -0,0 +1,57 @@
+export const newsItems = [
+  {
+    id: 1,
+    heading: "Dolor sed viverra ipsum nunc aliquet.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut diam quam nulla porttitor. Et ultrices neque ornare aenean euismod elementum. Sed libero enim sed faucibus turpis. Varius duis at consectetur lorem donec massa sapien. Habitant morbi tristique senectus et netus et malesuada fames ac. Dolor sed viverra ipsum nunc aliquet. Ut pharetra sit amet aliquam id diam maecenas ultricies mi. Urna neque viverra justo nec ultrices dui sapien eget mi. Aliquet enim tortor at auctor urna nunc.",
+    img:
+      "https://t4.ftcdn.net/jpg/02/89/99/33/240_F_289993382_QJID0KDhWhwp8JX3llcoD8V9jDKAZnpq.jpg",
+    imageDescription: "Sagittis nisl rhoncus mattis rhoncus urna neque",
+    blurb:
+      "Urna neque viverra justo nec ultrices dui sapien eget mi. Aliquet enim tortor at auctor urna nunc"
+  },
+  {
+    id: 2,
+    heading: "Lobortis elementum nibh tellus molestie nunc non blandit massa.",
+    content:
+      "Velit ut tortor pretium viverra suspendisse. Ullamcorper sit amet risus nullam eget felis. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Ultrices vitae auctor eu augue ut lectus arcu. Nulla porttitor massa id neque aliquam vestibulum morbi. Non blandit massa enim nec dui nunc mattis enim ut. Mattis nunc sed blandit libero volutpat. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Lorem ipsum dolor sit amet consectetur adipiscing. Cursus mattis molestie a iaculis at erat.",
+    img:
+      "https://theinspiredclassroom.com/wp-content/uploads/2017/05/students-singing.jpg",
+    imageDescription: "Eget nunc scelerisque viverra mauris",
+    blurb:
+      "Ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Lorem ipsum dolor sit amet consectetur adipiscing"
+  },
+  {
+    id: 3,
+    heading: "Dolor sed viverra ipsum nunc aliquet.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut diam quam nulla porttitor. Et ultrices neque ornare aenean euismod elementum. Sed libero enim sed faucibus turpis. Varius duis at consectetur lorem donec massa sapien. Habitant morbi tristique senectus et netus et malesuada fames ac. Dolor sed viverra ipsum nunc aliquet. Ut pharetra sit amet aliquam id diam maecenas ultricies mi. Urna neque viverra justo nec ultrices dui sapien eget mi. Aliquet enim tortor at auctor urna nunc.",
+    img:
+      "https://img3.stockfresh.com/files/c/courtyardpix/m/32/5694860_stock-photo-opera-singer-performing.jpg",
+    imageDescription: "Sagittis nisl rhoncus mattis rhoncus urna neque",
+    blurb:
+      "Et ultrices neque ornare aenean euismod elementum. Sed libero enim sed faucibus turpis."
+  },
+  {
+    id: 4,
+    heading: "Lobortis elementum nibh tellus molestie nunc non blandit massa.",
+    content:
+      "Velit ut tortor pretium viverra suspendisse. Ullamcorper sit amet risus nullam eget felis. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Ultrices vitae auctor eu augue ut lectus arcu. Nulla porttitor massa id neque aliquam vestibulum morbi. Non blandit massa enim nec dui nunc mattis enim ut. Mattis nunc sed blandit libero volutpat. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Lorem ipsum dolor sit amet consectetur adipiscing. Cursus mattis molestie a iaculis at erat.",
+    img:
+      "https://media.istockphoto.com/photos/phantom-of-the-opera-picture-id157181608?k=6&m=157181608&s=612x612&w=0&h=bU1CSw2KepfTSCY6rLkkd9Bhd0CRP-aJD-9zLddXuJM=",
+    imageDescription: "Eget nunc scelerisque viverra mauris",
+    blurb:
+      "Ullamcorper sit amet risus nullam eget felis. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia."
+  },
+  {
+    id: 5,
+    heading: "Magna sit amet purus gravida quis blandit turpis cursus in",
+    content:
+      "In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. In cursus turpis massa tincidunt dui. Augue neque gravida in fermentum et sollicitudin ac orci phasellus. Fermentum posuere urna nec tincidunt praesent semper feugiat. In ante metus dictum at tempor. Elementum curabitur vitae nunc sed velit dignissim sodales. Duis at consectetur lorem donec massa sapien faucibus. Orci a scelerisque purus semper. Nec tincidunt praesent semper feugiat nibh sed pulvinar.",
+    img:
+      "https://st.depositphotos.com/1258191/5177/i/450/depositphotos_51778961-stock-photo-conductor-directing-symphony-orchestra.jpg",
+    imageDescription: "Pharetra magna ac placerat vestibulum",
+    blurb:
+      "In cursus turpis massa tincidunt dui. Augue neque gravida in fermentum et sollicitudin ac orci phasellus."
+  }
+];
