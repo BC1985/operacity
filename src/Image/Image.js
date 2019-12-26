2f129bdcd18bd04f24154da861eae89fcecdeccf
Ben Cohen <benjcohen1985@gmail.com>
2019-12-26 11:57:20 -0500

    Wrap single image in container

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { urls } from "../Gallery/Gallery";
+import "./Image.css";
 
 const Image = () => {
   const { image } = useParams();
   const thisImage = urls.find(img => img.desc === image);
 
   return (
-    <div className="single-image">
-      <img src={thisImage.url} alt={thisImage.desc} />
+    <div className="single-image-wrapper">
+      <div className="single-image">
+        <img src={thisImage.url} alt={thisImage.desc} />
+      </div>
     </div>
   );
 };
