d17e52bcefeb44cc969f5740147634afebcfd8a0
Ben Cohen <benjcohen1985@gmail.com>
2019-12-26 11:56:40 -0500

    Replace images with placeholders Set logic for thumbnail class style

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -4,28 +4,64 @@ import "./Gallery.css";
 
 export const urls = [
   {
-    url: "https://live.staticflickr.com/65535/47710839562_0574dfd097_z.jpg",
+    url: "https://i.picsum.photos/id/508/2955/3892.jpg",
     desc: "opera1"
   },
   {
-    url:
-      "https://media.tacdn.com/media/attractions-splice-spp-674x446/07/69/50/2f.jpg",
+    url: "https://picsum.photos/id/832/5472/3648",
     desc: "opera2"
   },
   {
-    url:
-      "https://cdn.britannica.com/14/166714-050-454513F5/Performance-trovatore-Il-Giuseppe-Verdi-2011.jpg",
+    url: "https://i.picsum.photos/id/834/3644/5466.jpg",
     desc: "opera3"
   },
   {
-    url:
-      "https://www.airfrance.co.uk/GB/common/common/img/tbaf/news/NCE/l-opera-de-nice-la-traviatta-dans-son-decor-d-origine/NCE-l-opera-de-nice-la-traviatta-dans-son-decor-d-origine-2_1-1024x512.jpg",
+    url: "https://picsum.photos/id/836/5184/3456",
     desc: "opera4"
+  },
+  {
+    url: "https://i.picsum.photos/id/209/1920/1280.jpg",
+
+    desc: "opera5"
+  },
+  {
+    url: "https://picsum.photos/id/76/4912/3264",
+
+    desc: "opera6"
+  },
+  {
+    url: "https://i.picsum.photos/id/1033/2048/1365.jpg",
+
+    desc: "opera7"
+  },
+  {
+    url: "https://i.picsum.photos/id/1054/3079/1733.jpg",
+    desc: "opera8"
+  },
+  {
+    url: "https://i.picsum.photos/id/1059/7360/4912.jpg",
+    desc: "opera9"
+  },
+  {
+    url: "https://i.picsum.photos/id/320/2689/1795.jpg",
+    desc: "opera10"
+  },
+  {
+    url: "https://i.picsum.photos/id/1082/5416/3611.jpg",
+    desc: "opera11"
+  },
+  {
+    url: "https://i.picsum.photos/id/117/1544/1024.jpg",
+    desc: "opera12"
   }
 ];
-function Gallery() {
-  const thumbnails = urls.map(thumbnail => (
-    <div key={thumbnail.desc}>
+
+const getClass = i => {
+  return i % 2 === 0 ? "-tall" : "-normal";
+};
+const Gallery = () => {
+  const thumbnails = urls.map((thumbnail, i) => (
+    <div key={thumbnail.desc} className={`img-container${getClass(i)}`}>
       <Link to={`/gallery/${thumbnail.desc}`}>
         <img
           src={thumbnail.url}
@@ -41,6 +77,6 @@ function Gallery() {
       <div className="thumbnails-container">{thumbnails}</div>
     </div>
   );
-}
+};
 
 export default Gallery;
