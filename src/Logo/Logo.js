403bd9203e810b45e01d666369481db9b901c2b1
Ben Cohen <benjcohen1985@gmail.com>
2019-12-30 11:54:36 -0500

    Move logo to separate component

---
+++ src/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <i className="fas fa-theater-masks" id="logo"></i>
+      <h1 className="logo-header">Operacity</h1>
+    </div>
+  );
+};
+
+export default Logo;
