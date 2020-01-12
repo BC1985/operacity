972f962725a562689984725c49f6a5511b230e1d
Ben Cohen <benjcohen1985@gmail.com>
2020-01-11 19:11:41 -0500

    Remove sponsors

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,35 +1,6 @@
 import React from "react";
 import "./Footer.css";
 function Footer() {
-  const sponsersData = [
-    {
-      img:
-        "https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png",
-      name: "placeholder"
-    },
-    {
-      img:
-        "https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo2.png",
-      name: "placeholder"
-    },
-    {
-      img:
-        "https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.png",
-      name: "placeholder"
-    },
-    {
-      img:
-        "https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo4.png",
-      name: "placeholder"
-    }
-  ];
-  const sponsors = sponsersData.map((sponsor, index) => {
-    return (
-      <li key={index}>
-        <img src={sponsor.img} alt={sponsor.name} />
-      </li>
-    );
-  });
   return (
     <footer className="footer">
       <ul className="col-1">
@@ -43,11 +14,6 @@ function Footer() {
         <li>Volunteer</li>
         <li>Host</li>
         <li>Join</li>
-      </ul>
-
-      <ul className="sponsors">
-        <p style={{ marginBlockStart: "0" }}>Sponsors</p>
-        {sponsors}
       </ul>
       <ul className="social-media-footer">
         <li>
